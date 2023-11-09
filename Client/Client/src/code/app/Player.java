1181ca6dcd634d8cc4f36b543ecc31555110ab8e
henryda2004 <henryda2004@gmail.com>
2023-11-09 17:55:19 -0600

    Version Final

--- Client/Client/src/code/app/Player.java
+++ Client/Client/src/code/app/Player.java
@@ -9,9 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+import javax.swing.*;
 import java.util.Scanner;
 import java.io.*;
 import java.net.Socket;
@@ -246,6 +244,7 @@ public class Player extends JPanel implements ActionListener {
 
         if (lives == 0) {
             inGame = false;
+            JOptionPane.showMessageDialog(this, "¡Perdiste!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
         }
 
         continueLevel();
